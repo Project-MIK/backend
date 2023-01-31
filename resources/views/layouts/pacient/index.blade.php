f4916b2899d2ee198abfa09941378db2ebfadd33
aristocaesar <hi@aristoc.space>
2023-01-31 23:09:55 +0700

    init resource assets

---
+++ resources/views/layouts/pacient/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Telemedicine | Rumah Sakit Citra Husada Jember</title>
+
+  <link href="{{ asset('images/favicon.ico') }}" rel="shortcut icon">
+  <!-- Google Font: Poppins -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700;800&display=swap" rel="stylesheet">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+</head>
+
+<main class="wrapper">
+    <p>Hello World</p>
+</main>
+
+<!-- jQuery -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+</html>
