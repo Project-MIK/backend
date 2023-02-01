91021d695577ac7b4f566085a428189b9248445a
tajutzam <mohammadtajutzamzami07@gmail.com>
2023-02-01 20:40:44 +0700

    [add migrations database]

--- database/migrations/2023_02_01_133245_create_table_record.php
+++ database/migrations/2023_02_01_133245_create_table_record.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('record', function (Blueprint $table) {
+            $table->id();
+            $table->string('medical_record_id', 6)->nullable(false);
+            $table->string('description');
+            $table->string('complaint');
+            $table->unsignedBigInteger('id_recipe')->nullable(false);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('record');
     }
 };
