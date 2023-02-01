b4ad7ce1cff966e68c4475f5c96bf493318d039a
tajutzam <mohammadtajutzamzami07@gmail.com>
2023-02-01 20:47:47 +0700

    fix relation migration

--- database/migrations/2023_02_01_133245_create_table_record.php
+++ database/migrations/2023_02_01_133245_create_table_record.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('description');
             $table->string('complaint');
             $table->unsignedBigInteger('id_recipe')->nullable(false);
+            $table->foreign('medical_record_id')->references('medical_record_id')->on('medical_records')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('id_recipe')->references('id')->on('recipes')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
