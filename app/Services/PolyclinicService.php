5c9473c9732182e383c5c7d714c65f985c9d6604
sulthon <sulthon.abady28@gmail.com>
2023-03-03 01:24:37 +0700

    testing category done

--- app/Services/PolyclinicService.php
+++ app/Services/PolyclinicService.php
@@ -9,7 +9,7 @@ class PolyclinicService
 {
     public function findAll()
     {
-        $data = Polyclinic::with('categories')->orderBy('name')->get();
+        $data = Polyclinic::with('category')->orderBy('name')->get();
 
         if ($data->isEmpty()) {
             return null;
