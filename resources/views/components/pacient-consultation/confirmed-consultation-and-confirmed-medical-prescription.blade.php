5a1509a3b5b73f2e058975095741df16dcfabf28
aristocaesar <hi@aristoc.space>
2023-03-09 10:44:04 +0700

    [FIX] - fix response information pacient without buy medical

--- resources/views/components/pacient-consultation/confirmed-consultation-and-confirmed-medical-prescription.blade.php
+++ resources/views/components/pacient-consultation/confirmed-consultation-and-confirmed-medical-prescription.blade.php
@@ -15,71 +15,76 @@
             <div class="form-group col-12 text-right">
                 <a href="{{ $proofPaymentConsultation }}" target="_blank">CEK BUKTI PEMBAYARAN</a>
             </div>
+            <div class="alert alert-info w-100 font-weight-bold">
+                KONSULTASI TELAH SELESAI
+            </div>
         </div>
-        @if ($statusPaymentMedicalPrescription != "DIBATALKAN")
-            <div class="form-row">
-                <div class="form-group col-12">
-                    <label for="status-payment" class="text-trouth">Pembelian Obat</label>
-                </div>
-                <div class="form-group col-12 col-lg-6">
-                    <label for="price-medical-prescription" class="text-trouth font-weight-normal">Nominal Pembayaran</label>
-                    <input type="text" class="form-control py-4 font-weight-bold text-bunting" id="price-medical-prescription" value="{{ $priceMedicalPrescription }}" readonly>
-                </div>
-                <div class="form-group col-12 col-lg-6">
-                    <label for="status-payment-medical-prescription" class="text-trouth font-weight-normal">Status Pembayaran</label>
-                    <input type="text" class="form-control py-4" id="status-payment-medical-prescription" value="{{ $statusPaymentMedicalPrescription }}" readonly>
-                </div>
-                <div class="form-group col-12 text-right">
-                    <a href="{{ $proofPaymentMedicalPrescription }}" target="_blank">CEK BUKTI PEMBAYARAN</a>
-                </div>
-                <div class="form-group col-12">
-                    <label for="status-payment" class="text-trouth font-weight-normal">Status Pengambilan / Penerimaan Obat</label>
-                    <input type="text" class="form-control py-4" id="status-payment" value="{{ $pickupMedicalStatus }}" readonly>
-                </div>
-                @if ($pickupMedicalStatus != "SUDAH DIAMBIL")
-                    @if ($pickupMedicalPrescription == "hospital-pharmacy")
-                        <div class="form-group mt-2">
-                            <div class="col-12 d-flex flex-column mb-4">
-                                <label class="text-trouth">Dokumen Pengambilan Obat</label>
-                                <a href="/konsultasi/{{$id}}/export" target="_blank" class="mb-2">CETAK DOKUMEN PENGAMBILAN OBAT</a>
-                                <small>( Dokumen ini berguna sebagai syarat pengambilan obat )</small>
+        @if ($priceMedicalPrescription != null)            
+            @if ($statusPaymentMedicalPrescription != "DIBATALKAN")
+                <div class="form-row">
+                    <div class="form-group col-12">
+                        <label for="status-payment" class="text-trouth">Pembelian Obat</label>
+                    </div>
+                    <div class="form-group col-12 col-lg-6">
+                        <label for="price-medical-prescription" class="text-trouth font-weight-normal">Nominal Pembayaran</label>
+                        <input type="text" class="form-control py-4 font-weight-bold text-bunting" id="price-medical-prescription" value="{{ $priceMedicalPrescription }}" readonly>
+                    </div>
+                    <div class="form-group col-12 col-lg-6">
+                        <label for="status-payment-medical-prescription" class="text-trouth font-weight-normal">Status Pembayaran</label>
+                        <input type="text" class="form-control py-4" id="status-payment-medical-prescription" value="{{ $statusPaymentMedicalPrescription }}" readonly>
+                    </div>
+                    <div class="form-group col-12 text-right">
+                        <a href="{{ $proofPaymentMedicalPrescription }}" target="_blank">CEK BUKTI PEMBAYARAN</a>
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="status-payment" class="text-trouth font-weight-normal">Status Pengambilan / Penerimaan Obat</label>
+                        <input type="text" class="form-control py-4" id="status-payment" value="{{ $pickupMedicalStatus }}" readonly>
+                    </div>
+                    @if ($pickupMedicalStatus != "SUDAH DIAMBIL")
+                        @if ($pickupMedicalPrescription == "hospital-pharmacy")
+                            <div class="form-group mt-2">
+                                <div class="col-12 d-flex flex-column mb-4">
+                                    <label class="text-trouth">Dokumen Pengambilan Obat</label>
+                                    <a href="/konsultasi/{{$id}}/export" target="_blank" class="mb-2">CETAK DOKUMEN PENGAMBILAN OBAT</a>
+                                    <small>( Dokumen ini berguna sebagai syarat pengambilan obat )</small>
+                                </div>
+                                <div class="col-12">
+                                    <label class="text-trouth">Alamat Pengambilan</label>
+                                    <p>Jl. Teratai No.22, Gebang Timur, Gebang, Kec. Patrang, Kabupaten Jember, Jawa Timur 68117</p>
+                                    <a href="https://goo.gl/maps/53QjrwLKXPhQzccf8" target="_blank">LIHAT LOKASI</a>
+                                </div>
                             </div>
+                        @else
                             <div class="col-12">
-                                <label class="text-trouth">Alamat Pengambilan</label>
-                                <p>Jl. Teratai No.22, Gebang Timur, Gebang, Kec. Patrang, Kabupaten Jember, Jawa Timur 68117</p>
-                                <a href="https://goo.gl/maps/53QjrwLKXPhQzccf8" target="_blank">LIHAT LOKASI</a>
+                                <label for="pacient-notelp" class="text-trouth">Nomor Telepon</label>
+                                <input type="text" class="form-control py-4" id="pacient-notelp" name="pacient-notelp" value="{{ $pickupMedicalPhoneNumberPacient }}" readonly>
+                                
+                                <label for="pacient-addreass" class="text-trouth mt-3">Alamat Anda</label>
+                                <input type="text" class="form-control py-4" id="pacient-addreass" name="pacient-addreass" value="{{ $pickupMedicalAddreassPacient }}" readonly>
                             </div>
-                        </div>
+                            @if ($pickupMedicalStatus == "GAGAL DIKIRIM")
+                                <div class="alert alert-danger mt-3 col-12 p-4 d-flex flex-column" role="alert">
+                                    <p>{{ $pickupMedicalDescription }}</p>
+                                    <b>Silakan anda dapat mengambil obat secara mandiri di Rumah Sakit Citra Husada Jember.</b>
+                                </div>    
+                            @endif
+                        @endif  
                     @else
-                        <div class="col-12">
-                            <label for="pacient-notelp" class="text-trouth">Nomor Telepon</label>
-                            <input type="text" class="form-control py-4" id="pacient-notelp" name="pacient-notelp" value="{{ $pickupMedicalPhoneNumberPacient }}" readonly>
-                            
-                            <label for="pacient-addreass" class="text-trouth mt-3">Alamat Anda</label>
-                            <input type="text" class="form-control py-4" id="pacient-addreass" name="pacient-addreass" value="{{ $pickupMedicalAddreassPacient }}" readonly>
+                        <div class="col-12 text-trouth mt-3">
+                            <p>Obat sudah diambil oleh <strong>{{ strtoupper($pickupByPacient) }}</strong> pada <strong>{{ $pickupDatetime }}</strong> di Apotek Rumah Sakit Citra Husada Jember.</p>
                         </div>
-                        @if ($pickupMedicalStatus == "GAGAL DIKIRIM")
-                            <div class="alert alert-danger mt-3 col-12 p-4 d-flex flex-column" role="alert">
-                                <p>{{ $pickupMedicalDescription }}</p>
-                                <b>Silakan anda dapat mengambil obat secara mandiri di Rumah Sakit Citra Husada Jember.</b>
-                            </div>    
-                        @endif
-                    @endif  
-                @else
-                    <div class="col-12 text-trouth mt-3">
-                        <p>Obat sudah diambil oleh <strong>{{ strtoupper($pickupByPacient) }}</strong> pada <strong>{{ $pickupDatetime }}</strong> di Apotek Rumah Sakit Citra Husada Jember.</p>
-                    </div>
-                @endif
-            </div>
-        @else
-            <div class="form-row">
-                <div class="form-group col-12">
-                    <label for="status-payment" class="text-trouth">Status Pembelian Obat</label>
-                    <div class="alert alert-info font-weight-bold">
-                        {{$statusPaymentMedicalPrescription}}
+                    @endif
+                </div>
+            @else
+                <div class="form-row">
+                    <div class="form-group col-12">
+                        <label for="status-payment" class="text-trouth">Status Pembelian Obat</label>
+                        <div class="alert alert-info font-weight-bold">
+                            {{$statusPaymentMedicalPrescription}}
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         @endif
     @else
         <div class="form-row">
