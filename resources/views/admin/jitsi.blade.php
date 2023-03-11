98c5d71a467c1e2bcf772b4abaea8112aa587bc8
BachtiarAH <bachtiarah73@gmail.com>
2023-03-11 19:23:36 +0700

    dropdown untuk obat dan post ajax untuk receipt

--- resources/views/admin/jitsi.blade.php
+++ resources/views/admin/jitsi.blade.php
@@ -1,84 +1,61 @@
-{{-- 
+{{--
     TODO: membuat dropdown untuk obat, yang akan mengset harganya dari obatnya juga,
     TODO: mebuat qtynya bisa mengalikan harga dengan dirinya dan mengset valuenya ke sub total
     TODO: membuat fungsi ajax yang akan langsung mengirimkan data obat ke server, dan ke table
     --}}
 
 @extends('layouts.admin.AppOnlyAsset')
+<style>
+    .select2-results__option[aria-selected=false] {
+        color: #343a40;
+    }
+
+</style>
 @section('content')
-<div class="bg-white container-xl border rounded  m-5 mx-auto">
+<div class="bg-white border m-5 rounded mx-auto">
     <div class="row">
         <div class="col">
             <h3>Dokte : {{$data['doctor']}}</h3>
             <h5>Pasien : {{$data['patien']}}</h5>
+
         </div>
         <div class="col-3 ml-auto">
             <p class="text-mute">Waktu Konsultasi</p>
             <p id="countdown">00:00</p>
         </div>
     </div>
-    <div class="rounded h-80" style="height: 90%">
-        <div class="">
-            <div class="">
-                <div id="meet" class=""></div>
+    <div class="rounded" style="height: 100%">
+        <div class="row rounded " style="height: 90%">
+            <div class="col-9">
+                <div id="meet"></div>
             </div>
-            <div class="">
+            <div class="col-3">
                 <div class="card card-primary ">
                     <div class="card-header">
                         <h3 class="card-title">Tambah resep</h3>
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form action="/admin/consul/receipt/store" method="POST">
+                    <form action="/admin/consul/receipt/store" id="form-medicine" method="POST">
                         @csrf
                         <div class="card-body">
+
                             <div class="form-group">
-                                <label>Deskripsi</label>
-                                <textarea class="form-control" rows="3" placeholder="" name="description"></textarea>
+                                <label for="medicine">Obat</label>
+                                <select id="id_medicine" class="select2 form-control" name="medicine">
+                                    @foreach($medicine as $item)
+                                    <option value="{{$item['id']}}" data-price="{{$item['price']}}">
+                                        <p class="text-dark">{{$item['name']}}</p>
+                                    </option>
+                                    @endforeach
+                                </select>
                             </div>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <!-- radio -->
-                                    <div class="form-group">
-                                        <label for="pickup">Pick Up</label>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="radio" name="pickup">
-                                            <label class="form-check-label">rumah sakit</label>
-                                        </div>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="radio" name="pickup" checked="">
-                                            <label class="form-check-label">gojek</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Alamat</label>
-                                        <textarea class="form-control" rows="3" placeholder="" name="address"></textarea>
-                                    </div>
-                                </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">qty</label>
+                                <input type="number" name="qty" oninput="" class="form-control" id="qty" placeholder="harga obat">
                             </div>
-                            <div class="row">
-                                <div class="form-group col">
-                                    <label for="exampleInputEmail1">Obat</label>
-                                    <input type="text" class="form-control" id="medicine" placeholder="nama obat">
-                                </div>
-                                <div class="form-group col">
-                                    <label for="exampleInputPassword1">qty</label>
-                                    <input type="number" oninput="setSubTotal()" class="form-control" id="qty" placeholder="harga obat">
-                                </div>
-                                <div class="form-group col">
-                                    <label for="exampleInputPassword1">harga</label>
-                                    <input type="number" oninput="setSubTotal()" class="form-control" id="price" placeholder="harga">
-                                </div>
-                                <div class="form-group col">
-                                    <label for="exampleInputPassword1">sub total</label>
-                                    <div class="row">
-                                        <div class="col"><input type="number" class="form-control" id="sub-total" placeholder="subtotal"></div>
-                                        <div class="col"><button type="button" class="btn btn-primary" onclick="addData()">add</button></div>
-                                    </div>
-                                </div>
-                            </div>
+                            <div class="col"><button onclick="submitForm()" type="button" class="btn btn-primary">add</button></div>
+
                             <div class="alert alert-danger" role="alert" id="af" hidden>
                                 Form obat, qty, harga, dan sub total tidak boelh kosong
                             </div>
@@ -89,11 +66,20 @@
                                             <th>Obat</th>
                                             <th>qty</th>
                                             <th>harga</th>
-                                            <th>sub indo</th>
+                                            <th>total</th>
                                             <th></th>
                                         </tr>
                                     </thead>
                                     <tbody id="table-value">
+                                        @foreach($receipt as $item)
+                                        <tr>
+                                            <td>{{$item['name']}}</td>
+                                            <td>{{$item['qty']}}</td>
+                                            <td>{{$item['price']}}</td>
+                                            <td>{{$item['total']}}</td>
+                                            <td><button onclick='deleteRow(this)' type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button></td>
+                                        </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
@@ -130,7 +116,7 @@
     const options = {
         roomName: 'JitsiMeetAPIExample'
         , width: '100%'
-        , height: 700
+        , height: '100%'
         , parentNode: jitsiParentnode
         , lang: 'en'
         , userInfo: {
@@ -138,6 +124,12 @@
         }
     };
     const api = new JitsiMeetExternalAPI(domain, options);
+    api.on('readyToClose', () => {
+        console.log('The readyToClose event has been triggered');
+        api.dispose();
+        jitsiParentnode.innerHTML = "<div class='mx-auto'><h1>Sesei Konsultasi Telah Berakhir</h1><a href='/admin/consul/'><button class='col detail btn btn-block btn-primary btn-sm'>Kembali ke Page Konsultasi</button></a></div>";
+
+    });
 
 
     // Mendapatkan elemen yang akan menampilkan hitung mundur
@@ -173,12 +165,7 @@
         }
     }, 1000);
 
-    api.on('readyToClose', () => {
-        console.log('The readyToClose event has been triggered');
-        api.dispose();
-        jitsiParentnode.innerHTML = "<div class='mx-auto'><h1>Sesei Konsultasi Telah Berakhir</h1><a href='/admin/consul/'><button class='col detail btn btn-block btn-primary btn-sm'>Kembali ke Page Konsultasi</button></a></div>";
 
-    });
 
     function addMed() {
         var medicine = document.getElementById('medicine').value;
@@ -224,7 +211,7 @@
         cell5.innerHTML += subTotal;
         cell6.innerHTML = "<button onclick='deleteRow(this)' type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"
 
-        
+
 
 
         LastRow++;
@@ -232,19 +219,19 @@
     }
 
     function reArrangeCell() {
-        
+
         table = document.getElementById("table-value");
         rows = table.rows;
         console.log(rows);
 
         rows.forEach(element => {
             index = element.rowIndex;
-            console.log("row index = "+index);
+            console.log("row index = " + index);
 
             inputs = element.getElementsByTagName("input");
             console.log(inputs);
             inputs.forEach(element => {
-                element.name = "medicine["+(index-1)+"][]";
+                element.name = "medicine[" + (index - 1) + "][]";
             });
         });
     }
@@ -300,6 +287,36 @@
         subTotal.value = "";
     }
 
+    $(function() {
+        //Initialize Select2 Elements
+        $(".select2").select2();
+    });
+
+
+    function submitForm() {
+        var id_medicine = document.getElementById("id_medicine").value;
+        var qty = document.getElementById("qty").value;
+
+        // Kirim data menggunakan AJAX
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', "{{ route('receipt.store') }}", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('X-CSRF-TOKEN', "{{ csrf_token() }}");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        var data = JSON.stringify({
+            id_medicine: id_medicine
+            , qty: qty
+        });
+        xhr.send(data);
+
+        console.log("sended");
+    }
+
 </script>
+
 
 @endsection
--- resources/views/admin/pasien.blade.php
+++ resources/views/admin/pasien.blade.php
@@ -120,8 +120,9 @@
     $(function() {
         $("#example1").DataTable({
             "responsive": true
+            ,"order":[1,"asc"]
             , "lengthChange": false
-            , "autoWidth": false
+            , "autoWidth": true
             , "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
         }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0) ');
     });
