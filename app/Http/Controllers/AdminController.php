d1237c86455bd355bf599c4a84a1d15e2080dcc6
tajutzam <mohammadtajutzamzami07@gmail.com>
2023-05-07 18:49:00 +0700

    staging file

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -126,7 +126,7 @@ class AdminController extends Controller
      */
     public function destroy(Admin $admin)
     {
-       
+      
         $res = $this->service->deleteById($admin->id);
         if ($res) {
             // success deelte
